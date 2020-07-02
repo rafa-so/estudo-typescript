6669932401a512c02cc1b2697abd451012d0f36e
Rafael Oliveira <rafael.oliveira@spot-edu.com.br>
2020-07-02 12:22:11 -0300

    Finished typescript interview

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import express from 'express';
+import { helloWorld } from './routes';
 
 const app = express();
 
-
-app.get('/', (request, response) => (
-    response.json({ message: 'Hello World!' })
-))
-    
+app.get('/', helloWorld);
     
 app.listen(3333);
